27a1cc9f1a5ed47a3e4000fd5202e0e32c718e1a
Robson <robsontaveira37@gmail.com>
2025-07-21 13:17:19 -0300

    Prepara projeto para deploy com build.sh e settings de produção

--- comidas_do_chefe/settings.py
+++ comidas_do_chefe/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o4s66=o#9_0zu+kj=+h^l+@!=sv86=&+6j*we@sz&-%^2e&i5q'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'deixaeupensarvai')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = []
 
