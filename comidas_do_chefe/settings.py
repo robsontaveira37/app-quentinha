26d9265b66472d56b3803e7994ac68d103571fb5
Robson <robsontaveira37@gmail.com>
2025-07-21 17:01:11 -0300

    Adiciona a configuração STATI_ROOT para produção

--- comidas_do_chefe/settings.py
+++ comidas_do_chefe/settings.py
@@ -18,10 +18,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/python b
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-o4s66=o#9_0zu+kj=+h^l+@!=sv86=&+6j*we@sz&-%^2e&i5q')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-o4s66=o#9_0zu+kj=+h^l+@!=sv86=&+6j*we@sz&-%^2e&i5qS')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
@@ -118,7 +118,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
